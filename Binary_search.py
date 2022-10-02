e3b414f3e2f79df85813cb9e0255f9575a2149bb
ksj14-kumar <lionmassymassy@gmail.com>
2022-10-02 23:22:40 +0530

    Binary_search

---
+++ Binary_search.py
@@ -0,0 +1,14 @@
+# create a bnary serach algorithm
+
+
+def Binary_search(array):
+
+    l= len(array)
+
+    for i in array:
+        print(i)
+
+
+
+a= [12,1,3454,67,9,0,23,53,500]
+Binary_search(a)
